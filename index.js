d8a5952d0e975f9067900786c957f930bce4c10a
NexosBE <42642535+NexosBE@users.noreply.github.com>
2018-08-24 01:54:17 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+bot.login("process.env.TOKEN")
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setPresence({ game: { name: 'https://www.destinylife.fr/', type: 2} })
+    console.log("Connected");
+});
+bot.on("message", async function(message) {
+    if (message.author.equals(bot.user)) return;
+
+    if (!message.content.startsWith(prefix)) return;
+
+    var args = message.content.substring(prefix.length).split(" ");
+
+
+    switch(args[0].toLowerCase()) {
+        case "unknown":
+        message.channel.send("", {
+            embed: {
+                color: 0xFF0000,
+                author: message.author.name,
+                title: '',
+                fields: [{
+                    name: "**__Lien d'invitation discord__**",
+                    value: "[**Clique droit => Copier**](https://discord.gg/RJcxGct)",
+                    inline: false
+                }],
+                footer: {
+                    footer: "Partager ce lien a tous vos amis !",
+                },
+            }
+        });
+        break;
+    }
+});
